ca60a6cb32d6955ba1688b35bad9ac11d232821f
Kavya362000 <kavya.kurapati36@gmail.com>
2026-02-26 15:07:19 +0530

    Testing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Github Actions CI/CD Running Successfully!!!"
+    return "Version 2 deployed automatically!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
