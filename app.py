6f6de7ba24696741cc9f06eeee94eb2c849de7f8
Kavya362000 <kavya.kurapati36@gmail.com>
2026-02-26 14:32:21 +0530

    Updated message for Github

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Pipeline worked Successfully!!!"
+    return "Github Actions CI/CD Running Successfully!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
